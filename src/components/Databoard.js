fca2df1d018d95fe97a900d27314586e1ff91cdc
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-04 19:11:15 +0100

    set keys & states for data

--- src/components/Databoard.js
+++ src/components/Databoard.js
@@ -3,7 +3,26 @@ import getData from "../getData";
 import Results from "./Results";
 
 export default function Databoard() {
+  // set KEYS to indicate diff. time sections
+  const FIRST_HALF_KEY = 'fh';
+  const SECOND_HALF_KEY = 'sh';
+  const FULL_KEY = 'value';
+
+  // set state variables to store data to be displayed
   const [data, setData] = useState(null);
+
+  const [homePossessionData, setHomePossessionData] = useState([]);
+  const [awayPossessionData, setAwayPossessionData] = useState([]);
+  
+  const [homeShotData, setHomeShotData] = useState([]);
+  const [awayShotData, setAwayShotData] = useState([]);
+  
+  const [homeShotOnTargetData, setHomeShotOnTargetData] = useState([]);
+  const [awayShotOnTargetData, setAwayShotOnTargetData] = useState([]);
+  
+  const [homeCornersData, setHomeCornersData] = useState([]);
+  const [awayCornersData, setAwayCornersData] = useState([]);
+
 
   useEffect(() => {
     console.log("Fetching data");
