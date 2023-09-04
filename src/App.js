c58488157a23260325aa1e12922d3eb37d92e692
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-04 16:39:45 +0100

    create main components & add static data file

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Banner from "./components/Banner";
+import Menu from "./components/Menu";
+import ButtonList from "./components/ButtonList";
+import Databoard from "./components/Databoard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Banner />
+      <Menu />
+      <ButtonList />
+      <Databoard />
     </div>
   );
 }
---
+++ src/components/Databoard.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Databoard() {
+  return (
+    <div>Databoard</div>
+  )
+}
