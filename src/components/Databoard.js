2abd51778ef1777139e4586ec32425c2ab0f4356
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-04 17:06:00 +0100

    display data after fetching

--- src/components/Databoard.js
+++ src/components/Databoard.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import getData from "../getData";
 
 export default function Databoard() {
-  return (
-    <div>Databoard</div>
-  )
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    console.log("Fetching data");
+    setData(getData());
+  }, []);
+  return <div>{data ? data.match.id : "Fetching.."}</div>;
 }
