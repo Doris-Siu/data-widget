3d92a813376be89b2d0fc50d7fa3d94ab3382073
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-04 16:42:53 +0100

    update Menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -2,6 +2,14 @@ import React from 'react'
 
 export default function Menu() {
   return (
-    <div>Menu</div>
+    <div>
+        <ul>
+          <li>Overview</li>
+          <li>General</li>
+          <li>ARS</li>
+          <li>FUL</li>
+          <li>Player</li>
+        </ul>
+      </div>
   )
 }
