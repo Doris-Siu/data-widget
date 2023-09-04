65fd1b6d5e6bfb88d4fa47b94e1d2b814a9ed82a
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-09-04 16:42:02 +0100

    update Banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import asllogo from "../assets/arsenal.png";
+import fhlogo from "../assets/ffc.png";
 
 export default function Banner() {
   return (
-    <div>Banner</div>
-  )
+    <div>
+    <h1>Premier League</h1>
+    <div>
+      <img src={asllogo} alt="Arsenal logo" />
+      <span>Arsenal</span>
+    </div>
+    <div>
+      <img src={fhlogo} alt="Fulham logo" />
+      <span>Fulham</span>
+    </div>
+  </div>  )
 }
